6e3517e4120202efaf6af35de9cb5e90727b218f
Bartłomiej Kłuś <bartlomiej.klus@microsoft.wsei.edu.pl>
2023-10-14 09:02:52 +0200

    init

---
+++ bookApp/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace bookApp.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+    }
+}
