e67fdb187f6ba7aee649f8f9d77c4b33c27fef80
Bartłomiej Kłuś <bartlomiej.klus@microsoft.wsei.edu.pl>
2023-10-14 09:30:28 +0200

    secondpush

---
+++ bookApp/Controllers/BooksController.cs
@@ -0,0 +1,93 @@
+﻿using bookApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: BooksController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BooksController/Details/5
+
+        // GET: BooksControler/Random
+
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Autor = "Random author", Title = "random Title" };
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "title"});
+
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BooksController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BooksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
